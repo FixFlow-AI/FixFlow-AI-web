2430285387219b3f11c6e0e5a32b2aff190b89b3
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-07-28 18:05:43 +0530

    fix: correct imports and set max_retry as 3 in gemini_provider.py and groq_provider.py

--- ai-service/app/llm/providers/gemini_provider.py
+++ ai-service/app/llm/providers/gemini_provider.py
@@ -1,6 +1,6 @@
-from ..llm.provider.base import BaseLLMProvider
+from ...config import get_settings
+from ...llm.providers.base import BaseLLMProvider
 from langchain_google_genai import ChatGoogleGenerativeAI
-
 
 
 class GeminiProvider(BaseLLMProvider):
@@ -8,11 +8,12 @@ class GeminiProvider(BaseLLMProvider):
 
     def __init__(self):
 
+        settings = get_settings()
         self.llm = ChatGoogleGenerativeAI(
-            api_key=None,
-            model=None,
+            api_key=settings.gemini_api_key,
+            model=settings.gemini_model,
             temperature=0,
-            timeout=None,
+            timeout=settings.gemini_timeout_sec,
             max_retries=3,
         )
 
--- ai-service/app/llm/providers/groq_provider.py
+++ ai-service/app/llm/providers/groq_provider.py
@@ -1,5 +1,6 @@
+from ...config import get_settings
 from langchain_groq import ChatGroq
-from ..llm.provider.base import BaseLLMProvider
+from ...llm.providers.base import BaseLLMProvider
 
 
 
@@ -7,12 +8,13 @@ class GroqProvider(BaseLLMProvider):
     
 
     def __init__(self):
-        
+
+        settings = get_settings()
         self.llm = ChatGroq(
-            api_key=None,
-            model=None,
-            temperature=temperature,
-            timeout=None,
+            api_key=settings.groq_api_key,
+            model=settings.groq_model,
+            temperature=0,
+            timeout=settings.gemini_timeout_sec,
             max_retries=3,
         )
 
